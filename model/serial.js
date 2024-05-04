6dbe9114c53ceeeefdb0c6ff0f480342df1165a0
Yogineemane <157476176+Yogineemane@users.noreply.github.com>
2024-05-04 07:49:28 -0700

    one new model were added

---
+++ model/serial.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const serialSchema = new mongoose.Schema({
+    name:{
+        type : String,
+        require:true
+    },
+    rating:{
+        type : String,
+        require:true
+    },
+    description:{
+        type : String,
+        require : true,
+    },
+    picture:{
+        type :String,
+        require : true
+    },
+    isActive:{
+        type:Boolean,
+        default:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    }
+})
+module.exports=mongoose.model('serials',serialSchema)
