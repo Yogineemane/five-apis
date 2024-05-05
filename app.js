3a3fc39bebe491968ecc9aa78edf0a0d868c09e6
Yogineemane <157476176+Yogineemane@users.noreply.github.com>
2024-05-05 06:41:01 -0700

    created new file for new api

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
 require("dotenv").config()
-const express = require("express")
+const express = require('express')
 const mongoose = require('mongoose')
-const cors = require("cors")
-const MovieRoute = require("./route/movieRoute")
-const SerialRoute = require("./route/serialRoute")
+const cors = require('cors')
+const ProductRoute = require('./route/productRoute')
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.get("/",(req,res)=>{
-    res.send("Welcome")
+app.get('/',(req,res)=>{
+    res.send("HELLO")
 })
 
-app.use('/api/movie',MovieRoute)
-app.use('/api/serial',SerialRoute)
+app.use('/api/products1',ProductRoute)
 
 app.listen(process.env.PORT || 5000)
 
 async function mongoConnection() {
-    try {
-        const res = await mongoose.connect(process.env.DB)
-        const data = await res.default
-        console.log(data.STATES.connected);
-    } catch (error) {
-        console.log("Error",error.message);
-    }
-}
+   try {
+    const res = await mongoose.connect(process.env.DB)
+    const data = await res.default
+    console.log(data.STATES.connected);
+   } catch (error) {
+    console.log("Error",error.message);
+   }
+} 
 mongoConnection()
